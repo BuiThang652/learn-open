bf98489ad26b1a3f8083cb788733f85515f37486
Thanggg <buiducthang652@gmail.com>
2023-01-05 02:21:03 +0700

    day2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/header/header";
 import Banner from "./components/banner/banner";
+import Picture from "./components/picture/picture";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Picture />
     </div>
   );
 }
---
+++ src/components/picture/picture.js
@@ -0,0 +1,17 @@
+import "./picture.scss";
+
+const Picture = () => {
+  return (
+    <div className="picture">
+      <h1>Build up the whole picture</h1>
+      <p>
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui{" "}
+        <br></br>
+        officia deserunt mollit anim id est laborum — semper quis lectus{" "}
+        <br></br> nulla at volutpat diam ut venenatis.
+      </p>
+    </div>
+  );
+};
+
+export default Picture;
